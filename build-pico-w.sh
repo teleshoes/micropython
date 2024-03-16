39ee406a76304572f93c84d512072af58315e173
Elliot Wolk <elliot.wolk@gmail.com>
2024-03-15 22:09:37 -0400

    build: add build script for RPI_PICO_W + st7789_mpy

---
+++ build-pico-w.sh
@@ -0,0 +1,20 @@
+#sudo apt-get install gcc-arm-none-eabi
+
+git clean -f -d -X
+
+rm -f firmware.uf2
+
+echo building \
+  && make -C mpy-cross -j 8 \
+  && cd ports/rp2 \
+  && make BOARD=RPI_PICO_W clean \
+  && make submodules \
+  && make \
+        USER_C_MODULES=../../../st7789_mpy/st7789/micropython.cmake \
+        BOARD=RPI_PICO_W \
+        FROZEN_MANIFEST=../boards/RPI_PICO_W/manifest.py \
+        V=1 \
+        -j 8 \
+  && cd ../.. \
+  && cp -a ports/rp2/build-RPI_PICO_W/firmware.uf2 . \
+  && echo SUCCESS
