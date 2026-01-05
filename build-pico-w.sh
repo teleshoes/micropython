7e46c1aaa833f7eb8a6666e5fff926d276237626
Elliot Wolk <elliot.wolk@gmail.com>
2026-01-05 12:19:32 -0500

    build: pull build(BOARD_NAME) to a function

--- build-pico-w.sh
+++ build-pico-w.sh
@@ -2,21 +2,24 @@ sudo apt-get install gcc-arm-none-eabi libstdc++-arm-none-eabi-newlib
 
 rm -f firmware_*.uf2
 
-BOARD_NAME=RPI_PICO_W
-
 SKIP_CLEAN=N
 
-echo building \
-  && make -C mpy-cross -j 1 \
-  && cd ports/rp2 \
-  && if [ $SKIP_CLEAN = Y ]; then echo skip clean; else make BOARD=$BOARD_NAME clean; fi \
-  && make submodules BOARD=$BOARD_NAME \
-  && make \
-        USER_C_MODULES=../../lib/st7789_mpy/st7789/micropython.cmake \
-        BOARD=$BOARD_NAME \
-        FROZEN_MANIFEST=../boards/$BOARD_NAME/manifest.py \
-        V=1 \
-        -j 8 \
-  && cd ../.. \
-  && cp -a ports/rp2/build-$BOARD_NAME/firmware.uf2 firmware_$BOARD_NAME.uf2 \
-  && echo SUCCESS
+function build() {
+  BOARD_NAME=$1
+  echo building \
+    && make -C mpy-cross -j 1 \
+    && cd ports/rp2 \
+    && if [ $SKIP_CLEAN = Y ]; then echo skip clean; else make BOARD=$BOARD_NAME clean; fi \
+    && make submodules BOARD=$BOARD_NAME \
+    && make \
+          USER_C_MODULES=../../lib/st7789_mpy/st7789/micropython.cmake \
+          BOARD=$BOARD_NAME \
+          FROZEN_MANIFEST=../boards/$BOARD_NAME/manifest.py \
+          V=1 \
+          -j 8 \
+    && cd ../.. \
+    && cp -a ports/rp2/build-$BOARD_NAME/firmware.uf2 firmware_$BOARD_NAME.uf2 \
+    && echo SUCCESS
+}
+
+build "RPI_PICO_W"
