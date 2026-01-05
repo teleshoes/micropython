c970e9fc781bc2fac7986e949bd535f7d7dfeb9b
Elliot Wolk <elliot.wolk@gmail.com>
2026-01-05 00:25:51 -0500

    build: pull BOARD_NAME to a global var

--- build-pico-w.sh
+++ build-pico-w.sh
@@ -4,17 +4,19 @@ git clean -f -d -X
 
 rm -f firmware_*.uf2
 
+BOARD_NAME=RPI_PICO_W
+
 echo building \
   && make -C mpy-cross -j 1 \
   && cd ports/rp2 \
-  && make BOARD=RPI_PICO_W clean \
-  && make submodules BOARD=RPI_PICO_W \
+  && make BOARD=$BOARD_NAME clean \
+  && make submodules BOARD=$BOARD_NAME \
   && make \
         USER_C_MODULES=../../lib/st7789_mpy/st7789/micropython.cmake \
-        BOARD=RPI_PICO_W \
-        FROZEN_MANIFEST=../boards/RPI_PICO_W/manifest.py \
+        BOARD=$BOARD_NAME \
+        FROZEN_MANIFEST=../boards/$BOARD_NAME/manifest.py \
         V=1 \
         -j 8 \
   && cd ../.. \
-  && cp -a ports/rp2/build-RPI_PICO_W/firmware.uf2 firmware_RPI_PICO_W.uf2 \
+  && cp -a ports/rp2/build-$BOARD_NAME/firmware.uf2 firmware_$BOARD_NAME.uf2 \
   && echo SUCCESS
